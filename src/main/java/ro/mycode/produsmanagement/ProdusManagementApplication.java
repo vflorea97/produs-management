1dd24477c667acb2a67dcec708d8176d24dfa952
Valentin <vflorea97@yahoo.com>
2023-02-21 21:22:23 +0200

    create project produs-management

---
+++ src/main/java/ro/mycode/produsmanagement/ProdusManagementApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.produsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdusManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdusManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/produsmanagement/ProdusManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.produsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdusManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
